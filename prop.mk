1a7ded47bd73dba79cf53b289db8067041b87535
ItsVixano <vixanonew@gmail.com>
2020-12-24 18:04:20 +0000

    ysl: Decommonize dpi vaule
    * https://github.com/ItsVixano/android_device_xiaomi_msm8953-common/commit/8e17fcec92471ce66bbc92becba07259f0b2af4f
    
    Change-Id: Idf8af25fac711c6db278dc15d05e7d354decda75

--- prop.mk
+++ prop.mk
@@ -10,3 +10,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0
 
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.sf.lcd_density=320
