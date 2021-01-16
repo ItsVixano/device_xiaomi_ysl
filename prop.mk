21683aecec0de7cdf9268fff621f9ea85f0a0c73
Artem Borisov <dedsa2002@gmail.com>
2021-01-16 10:27:52 +0100

    ysl: Enable fluence
    Change-Id: I38a0c9fab5b123525348c8ac4bfc25828360c6ad

--- prop.mk
+++ prop.mk
@@ -1,3 +1,10 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.audio.fluence.speaker=true \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+ro.vendor.audio.sdk.fluencetype=fluence
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sf.lcd_density=320 \
