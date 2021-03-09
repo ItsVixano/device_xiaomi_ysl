5e8cfc28800e26eb260ec07b75d41d1ebc3b64e3
ItsVixano <vixanonew@gmail.com>
2021-03-09 08:42:13 +0100

    ysl: Import camera wraper from LA.UM.7.6.r1-03900-89xx.0
    Change-Id: Ib2c1500fd84545f8620fe538ccb073ba7c3b4e77

--- prop.mk
+++ prop.mk
@@ -5,6 +5,10 @@ persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=false \
 ro.vendor.audio.sdk.fluencetype=fluence
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.camera.dual.camera=0
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sf.lcd_density=320 \
