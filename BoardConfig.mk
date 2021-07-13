be58257888657da516d0395db6dd178b5cdab094
ItsVixano <vixanonew@gmail.com>
2021-07-13 13:23:49 +0200

    ysl: Inherit dummy Makefiles
    Change-Id: I6f4b82c7b669ec3dadd10741eb67a3e068614e4a

---
+++ BoardConfig.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_KERNEL_VERSION := 4.9
+
+# Inherit from common msm8953-common
+include device/xiaomi/msm8953-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/ysl
+
+# Kernel
+TARGET_KERNEL_CONFIG := ysl_defconfig
+
+# Partitions
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 25765043200
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
+
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+
+# Inherit the proprietary files
+include vendor/xiaomi/ysl/BoardConfigVendor.mk
