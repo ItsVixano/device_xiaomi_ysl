c5465b42f09b4c8557602ff08fc8a53673a356b7
ItsVixano <vixanonew@gmail.com>
2021-07-13 14:14:30 +0200

    ysl: Import audio configs
    * from V12.0.2.0.PEFMIXM
    
    Change-Id: I3790d2cc6111ec9d1fe5a50a280a58f873f80ed6

--- device.mk
+++ device.mk
@@ -28,6 +28,11 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
